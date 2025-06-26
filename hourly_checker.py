2ecc659a6fc2fec4401f4499f58defea74a16865
Shockk-xD <132000886+Shockk-xD@users.noreply.github.com>
2025-06-27 00:29:01 +0500

    Update hourly_checker.py

--- hourly_checker.py
+++ hourly_checker.py
@@ -1,13 +1,14 @@
+# hourly_checker.py
 import asyncio
+import os
 from check_logic import perform_price_check
 from telegram import Bot
-import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = int(os.getenv("CHAT_ID"))
 
 async def main():
-    msg, _ = perform_price_check()  # <--- бери только сообщение
+    msg, _ = perform_price_check()  # обязательно msg, _
     bot = Bot(token=BOT_TOKEN)
     await bot.send_message(chat_id=CHAT_ID, text=msg)
 
