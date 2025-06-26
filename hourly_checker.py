11a738d3191cfbf690050aff9616fd6c29581307
Shockk-xD <132000886+Shockk-xD@users.noreply.github.com>
2025-06-26 23:45:41 +0500

    Update hourly_checker.py

--- hourly_checker.py
+++ hourly_checker.py
@@ -1,10 +1,10 @@
 # hourly_checker.py
-# -*- coding: utf-8 -*-
+import os
 from check_logic import perform_price_check
 from telegram import Bot
 
-BOT_TOKEN = '7700315955:AAFqi6Bat6VFNoVTLEFKZo4Ac1WD8ROyv3Y'
-CHAT_ID   = 1085776764
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID   = int(os.getenv("CHAT_ID"))
 
 if __name__ == "__main__":
     msg, _ = perform_price_check()
